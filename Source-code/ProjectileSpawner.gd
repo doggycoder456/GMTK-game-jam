2adbc0524cc92868577f6c06bcd847c67e276eda
doggycoder456 <132857780+doggycoder456@users.noreply.github.com>
2024-08-17 01:22:57 -0500

    Update ProjectileSpawner.gd
    Signed-off-by: doggycoder456 <132857780+doggycoder456@users.noreply.github.com>

--- Source-code/ProjectileSpawner.gd
+++ Source-code/ProjectileSpawner.gd
@@ -3,9 +3,6 @@ extends Node2D
 @onready var main = get_tree().get_root().get_node("Main")
 @onready var projectile = load("res://Projectiles.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func _process(delta):
 	if Input.is_action_just_pressed("space"):
