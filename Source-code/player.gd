d4af095eccfd7050eb38b9291a745a288df31bcb
doggycoder456 <jakeu7782@gmail.com>
2024-08-16 21:02:31 -0500

    Inital files for godot added

---
+++ Source-code/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+
+@export var player_speed = 100
+var screensize;
+signal hit;
+
+func _ready():
+	screensize = get_viewport_rect().size
+	if (position.x != screensize.x / 4 && position.y != screensize.y / 2):
+		position.x = screensize.x / 4
+		position.y = (screensize.y / 2)
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * player_speed
+	
+	position += velocity * delta
+	#position = position.clamp(Vector2.ZERO, screensize)
+	print(position)
+
+
+
+
+func _on_collison_zone_2d_body_entered(body):
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
