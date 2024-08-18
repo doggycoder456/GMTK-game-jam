9a238dc22800632e2d8b93dc8536568b3a4ab074
doggycoder456 <jakeu7782@gmail.com>
2024-08-18 15:50:25 -0500

    added folder for ui art

--- Source-code/Main.gd
+++ Source-code/Main.gd
@@ -4,26 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	$Player.hide()
-	new_game() # Replace with function body.
-
-
-func game_over():
-	new_game()
-	$HUD.show_game_over()
-	$Player.hide()
-
-func new_game():
-	$Player.hide()
-	$HUD.hide()
-	$Player.start()
-	$HUD.show()
-	$HUD.show_message("Get Ready")
-	$Player.show()
+	$Menu.hide()
+	$Menu.get_tree().hide()
 
 func _process(delta):
 	pass
-
-
-func _on_player_hit():
-	$Player.current_player_health -= 10 # Replace with function body.
--- Source-code/Menu.gd
+++ Source-code/Menu.gd
@@ -9,3 +9,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_button_pressed():
+	print("Started Game") # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	print("Exited Game") # Replace with function body.
+
+
+
+func _on_settings_button_pressed():
+	print("Settings opened") # Replace with function body.
+
+
+
+func _on_credits_button_pressed():
+	print("Credits opened") # Replace with function body.
