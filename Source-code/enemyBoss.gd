067af1e589884e377706b3642e0a0c5bdbfa024f
Osama Hussein <osamahusseinn2@gmail.com>
2024-08-19 13:29:46 -0400

    Enemy health bar functional

--- Source-code/enemyBoss.gd
+++ Source-code/enemyBoss.gd
@@ -2,8 +2,12 @@ class_name enemyBoss
 
 extends Enemy
 
+@export var enemyBossHealth = 300
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$EnemyHealthBar.value = enemyBossHealth
+	
 	pass # Replace with function body.
 
 
